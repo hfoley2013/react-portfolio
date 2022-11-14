5ccb622c43b56c6a04f3a59310ad89db83ba65db
Harper Foley <harper.e.foley@gmail.com>
2022-11-14 05:35:09 +0000

    Adds GraphQL icon

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import graphqlIcon from "@iconify/icons-logos/graphql";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import pythonIcon from '@iconify/icons-logos/python';
 
 class About extends Component {
   render() {
@@ -30,16 +30,16 @@ class About extends Component {
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
+                    <Icon
+                      icon={pythonIcon}
+                      style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    />
+                    <Icon
+                      icon={reactIcon}
+                      style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    />
                   <Icon
-                    icon={angularIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={vueIcon}
+                    icon={graphqlIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                 </span>
